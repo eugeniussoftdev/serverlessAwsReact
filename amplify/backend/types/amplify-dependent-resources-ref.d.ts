8aa13aefd0a693c2f9c07114e5940348543e1497
macbook <macbook@MacBook-Pro-mac.local>
2023-06-08 20:53:38 +0300

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsreactapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "awsreactapp2f1f6901": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import logo from "./logo.svg";
+
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+
+const CenteredContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Ensure the container covers the full height */
+`;
+
+const Div = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: black;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p> */}
+        <CenteredContainer>
+          <Div />
+        </CenteredContainer>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreeting: true });
